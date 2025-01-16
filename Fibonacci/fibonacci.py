02f7b1de11d51c6b8da772bfb5bbbf43d4f72664
DANR1BEIRO <danribeiro001@gmail.com>
2025-01-16 11:34:34 -0300

    Fibo sequence

--- Fibonacci/fibonacci.py
+++ Fibonacci/fibonacci.py
@@ -1,29 +1,26 @@
 def fibonacci():
     """
-    A function that takes a number as input and displays
-    the Fibonacci sequence up to that number
+    Generate a Fibonacci sequence up to the user specified term
     """
     while True:
-        try: 
-            term = int(input("Fibonacci term: "))
-            if term <= 0:
-                print("Term must be greater than zero!")
-            else:
+        try:
+            term = int(input("Enter fibo term: "))
+            if 0 < term:
                 break
+            else:
+                print("Term must be greater than zero")
         except ValueError:
-            print("Term must be a integer number!")
-            
-    if term <= 2:
-        return [] if term <= 0 else [0] if term == 1 else [0, 1]
+            print("Term must be an integer number")
 
-    sequence = [0, 1]
-    
-    for x in range(2, term):
+    if term <= 2:
+        return [0] if term == 1 else [0,1]
+
+    sequence = [0,1]
+    for i in range(2, term):
         next_term = sequence[-1] + sequence[-2]
         sequence.append(next_term)
     return sequence
-        
-if __name__=="__main__":
-    result = fibonacci()
-    print(f"The fibonacci sequence up to {len(result)} terms is: {result}")
 
+if __name__=="__main__":
+    fibo = fibonacci()
+    print(f"The fibonacci sequence up to {len(fibo)} {'term' if len(fibo) == 1 else 'terms'} is {fibo}")
--- Fibonacci/fibonacci_cubed.py
+++ Fibonacci/fibonacci_cubed.py
@@ -4,23 +4,26 @@ cube = lambda x: x ** 3
 
 # The function shouldn't ask for input inside it if it already receives the number as an argument
 def fibonacci():
+    """
+    Generate a Fibonacci sequence up to the user specified term
+    """
     while True:
         try:
-            number = int(input("Fibonacci: "))
-            if number >= 0:
+            term = int(input("Fibonacci: "))
+            if 0 < term:
                 break
             else:
                 print("Number must be greater than 0!")
         except ValueError:
             print("Only integer numbers are allowed!")
 
-    if number <= 2:
-        return [] if number <= 0 else [0] if number == 1 else [0, 1]
+    if term <= 2:
+        return [0] if term == 1 else [0,1]
+    
     sequence = [0, 1] # starts with 0, 1
-    for x in range(2, number): # calculates from 2 up the desired position
+    for x in range(2, term): # calculates from 2 up the desired position
         next_value = sequence[-1] + sequence[-2] # adds the last two number of the sequence
-        sequence.append(next_value) # adds the sum of the last two numbers to the end of the list
-        
+        sequence.append(next_value) # adds the sum of the last two numbers to the end of the list     
     return sequence
 
 if __name__=="__main__":
