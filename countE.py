51d75f1367ca8a3c5393198d3ebfda59e74c4d21
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-05 13:23:59 -0300

    All multiples of 3 from 0 to 500

--- countE.py
+++ countE.py
@@ -1,17 +1,19 @@
 noE = ["A", "b", "C", "c", "z"]
-yesE = ["e", "M","z", "E", "e", "q"]
+yesE = ["e", "M","z", "E", "e", "q", "E", "E"]
 
 
 def findE(arr):
     counter = {"E":0, "e":0}
 
     for letter in arr:
-        counter[letter] = 1
+        if letter in counter:
+            counter[letter] += 1
 
     if counter["E"] == 0 and counter["e"] == 0:
         print("\nThere's no \"E\" or \"e\" letter")
     else:
-        print(f"{counter["E"]} letters E and {counter["e"]} letters e")
+        print(f"{counter["E"]} {"letters" if counter["E"] > 1 else "letter"} E") 
+        print(f"{counter["e"]} {"letters" if counter["e"] > 1 else "letter"} e")
 
 findE(yesE)
 findE(noE)
