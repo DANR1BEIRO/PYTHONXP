6e5d40fc2ecd95fbb60d98e83bcdabd05671249d
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-05 13:36:06 -0300

    sum of even in array

--- filtering_even_numbers.py
+++ filtering_even_numbers.py
@@ -1,8 +1,8 @@
-numbers = [1, 4, 12, 21, 53, 88, 112]
-even_numbers = []
+numbers = [3, 4, 7, 8, 1, 6, 5, 10]
+even_numbers = 0
 
 for num in numbers:
     if num % 2 == 0:
-        even_numbers.append(num)
-        
+        even_numbers += num
+
 print(even_numbers)
