6c4b4a2859b0b0d93175ddc136633430b342eda4
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-05 13:37:54 -0300

    Iterating through an array and filtering the even numbers.

--- filtering_even_numbers.py
+++ filtering_even_numbers.py
@@ -1,8 +1,8 @@
-numbers = [3, 4, 7, 8, 1, 6, 5, 10]
-even_numbers = 0
+numbers = [1, 4, 12, 21, 53, 88, 112]
+even_numbers = []
 
 for num in numbers:
     if num % 2 == 0:
-        even_numbers += num
-
+        even_numbers.append(num)
+        
 print(even_numbers)
