41c99ed6b1ce98a03d12cbfaf5c28f9ef2c888fc
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-06 14:15:57 -0300

    Return the index of an arr[value] if value == 10 else -1

--- index_of_value_in_arr.py
+++ index_of_value_in_arr.py
@@ -1,9 +1,15 @@
+"""""
+Return the index of an arr[value] if value == 10 else -1
+"""
+
+# 1
 arr = [1, 2, 3, 4, 10, -70]
 
 for i in range(len(arr)):
     if arr[i] == 10:
         print(i)
-        
+
+# 2    
 def findIndex(arr, target):
     for index, value in enumerate(arr):
         if value == target:
@@ -12,7 +18,9 @@ def findIndex(arr, target):
 
 print(findIndex(arr, 10))
 
+# 3
 def find_index(arr, target):
     return arr.index(target) if target == 10 else -1
 
 print(find_index(arr, 10))
+
