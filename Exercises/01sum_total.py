793aabc28b1a95836d403e56e4475a7a96fca5ff
DANR1BEIRO <danribeiro001@gmail.com>
2025-02-08 11:56:31 -0300

    Sum of all numbers in the array

---
+++ Exercises/01sum_total.py
@@ -0,0 +1,11 @@
+# create a program that takes an array of numbers, then
+# calculates and prints the sum of all numbers in the array
+
+numbers = [2, 3, 4, 6]
+count = 0
+
+for num in numbers:
+    count += num
+print(count)
+
+
--- sum.py
+++
@@ -1,8 +0,0 @@
-numbers = [2, 3, 4, 6]
-count = 0
-
-for num in numbers:
-    count += num
-print(count)
-
-
